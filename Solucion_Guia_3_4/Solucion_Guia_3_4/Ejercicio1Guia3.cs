1c2c8d4a726ecd347d32bc671eeab0e8cdadcf21
Jason Sosa <hernamdezjason36@gmail.com>
2021-09-01 12:22:41 -0600

    Creación de ventanas

---
+++ Solucion_Guia_3_4/Solucion_Guia_3_4/Ejercicio1Guia3.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Solucion_Guia_3_4
+{
+    public partial class Ejercicio1Guia3 : Form
+    {
+        public Ejercicio1Guia3()
+        {
+            InitializeComponent();
+        }
+    }
+}
---
+++ Solucion_Guia_3_4/Solucion_Guia_3_4/Ejercicio2Guia4.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Solucion_Guia_3_4
+{
+    public partial class Ejercicio2Guia4 : Form
+    {
+        public Ejercicio2Guia4()
+        {
+            InitializeComponent();
+        }
+    }
+}
