47980f6dcb7e86b9b8167b3fb462e02a9f6ad7c8
Jason Sosa <hernamdezjason36@gmail.com>
2021-09-01 13:37:16 -0600

    Ejercicio 1 guia 3 terminado

--- Solucion_Guia_3_4/Solucion_Guia_3_4/Ejercicio1Guia3.cs
+++ Solucion_Guia_3_4/Solucion_Guia_3_4/Ejercicio1Guia3.cs
@@ -12,9 +12,77 @@ namespace Solucion_Guia_3_4
 {
     public partial class Ejercicio1Guia3 : Form
     {
+        List<string> name = new List<string>();
         public Ejercicio1Guia3()
         {
             InitializeComponent();
+        }
+
+        private void Ejercicio1Guia3_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void BtnName_Click(object sender, EventArgs e)
+        {
+            name.Add(TxtName.Text);
+            TxtName.Clear();            
+        }
+
+        private void BtnMostrar_Click(object sender, EventArgs e)
+        {
+            DataName.Columns.Clear();
+            DataName.Rows.Clear();
+            DataName.Columns.Add("Name", "Name");
+            foreach (String item in name)
+            {
+                DataName.Rows.Add(item);
+            }
+            
+        }
+
+        private void BtnSiguiente_Click(object sender, EventArgs e)
+        {
+            this.Hide();
+            Ejercicio2Guia3 eje2guia3 = new Ejercicio2Guia3();
+            eje2guia3.Show();
+        }
+
+        private void BtnSalir_Click(object sender, EventArgs e)
+        {
+            this.Hide();
+            Form1 form = new Form1();
+            form.Show();
+        }
+
+        private void BtnEliminar_Click(object sender, EventArgs e)
+        {
+            name.Remove(TxtEliminar.Text);
+            DataName.Columns.Clear();
+            DataName.Rows.Clear();
+            DataName.Columns.Add("Name", "Name");
+            foreach (String item in name)
+            {
+                DataName.Rows.Add(item);
+            }
+            TxtEliminar.Clear();
+
+        }
+
+        private void BtnModName_Click(object sender, EventArgs e)
+        {
+            var indice = name.IndexOf(TxtNameOld.Text);
+            name.RemoveAt(indice);
+            name.Insert(indice,TxtNameNew.Text);
+            DataName.Columns.Clear();
+            DataName.Rows.Clear();
+            DataName.Columns.Add("Name", "Name");
+            foreach (String item in name)
+            {
+                DataName.Rows.Add(item);
+            }
+            TxtNameNew.Clear();
+            TxtNameOld.Clear();
         }
     }
 }
